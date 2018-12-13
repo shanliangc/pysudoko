beca78754bc0f547723f29d3443853ea7c74a433
“shanliangc” <948452033@qq.com>
2018-12-13 17:21:39 +0800

    a version to test constant and variable in pylanguage

--- sudoko/test2.py
+++ sudoko/test2.py
@@ -18,3 +18,7 @@ for i in range(20):
     a = student_table(i)
     print(a)
     a.print_table()
+
+#在python的学习中发现了一新的，曾经没有注意过的内容：
+#python中的类没有java中的前缀static等，只要写在class里面的，都为static
+#而在函数中使用的self.变量名 才是真正的每个python类的实例化拥有的私有变量
